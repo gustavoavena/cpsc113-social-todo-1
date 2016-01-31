39ea361bf6f6ef332190fe84c804a8185605b295
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-01-30 22:23:12 -0500

    handles sessions

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var app = express();
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var $ = require('jQuery');
 
-var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -15,40 +16,76 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var Users = require('./models/users.js');
 
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: 'auto' }
+}));
+
+
+app.use(function(req, res, next) {
+
+    console.log('req.session = ', req.session);
+    if(req.session.userId) {
+        Users.findById(req.session.userId, function(err, user) {
+            if(!err) {
+                res.locals.currentUser = user;
+            }
+            next();
+        });
+    }
+    else {
+        next();
+    }
+    
+    }
+);
+
 // Configuratio above
 
 app.get('/', function (req, res) {
-    // res.render('home');
+    // res.render('index');
 
     Users.find({}, function(err, users){
         if(err) {
-            res.send("Error getting users from database");
+            return res.send("Error getting users from database");
         }
         else {
             // res.send(String(users.length));
-            res.render('home',{userCount : users.length});
+            res.render('index',{
+                userCount : users.length,
+                currentUser: res.locals.currentUser
+            });
         }
     });
 });
 
 
 app.post('/user/register', function (req, res) {
+    if(req.body.password !== req.body.password_confirmation) {
+        return res.render('index', {errors: 'Passwords dont match.'});
+    }
+
 
     var newUser = new Users();
 	newUser.name = req.body.fl_name;
 	newUser.email = req.body.email;
 	newUser.password = req.body.password;
 
-	newUser.save(function (err) {
+	newUser.save(function (err, user) {
 		if(err) {
 			console.log('Error saving user to database');
-			console.log(err);
+			console.log(err.errors);
+            return res.render('index', { errors: err} );
 		}
 		else {
+            req.session.userId = user._id;
 			console.log('User registered');
+            res.redirect('/');
+
 		}
 	});
- 	res.redirect('/');
     // res.send(new_name); Tava dando erro pq nao pode dar res.send nada dps de renderizar header.
  //    res.render('dashboard');
 	// }
