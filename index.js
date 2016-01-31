a960e2203597dcc83e73fff4235bde839286ade3
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-01-30 20:20:07 -0500

    merged

--- index.js
+++ index.js
@@ -11,34 +11,13 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
 
 var Users = require('./models/users.js');
 
 
 // Configuratio above
 
-=======
->>>>>>> 3751149853d40f6fffd4876508015a2d23a19f5f
-app.get('/', function (req, res) {
-    // res.render('home');
 
-    Users.find({}, function(err, users){
-    	if(err) {
-    		res.send("Error getting users from database");
-    	}
-    	else {
-    		// res.send(String(users.length));
-    		res.render('home',{userCount : users.length});
-    	}
-    });
-});
-
-app.post('/signup', function (req, res) {
-    res.render('signup_view');
-});
-
-<<<<<<< HEAD
 app.post('/user/register', function (req, res) {
 
     var newUser = new Users();
@@ -60,21 +39,6 @@ app.post('/user/register', function (req, res) {
  //    res.render('dashboard');
 	// }
 });
-=======
-app.post('/signup', function (req, res) {
-    res.render('signup_view');
-});
-
-app.post('/user/register', function (req, res) {
-    // res.send('sign up values');
-    var new_name = req.body.name;
-    // var email = String(req.body.email);
- 	console.log('chegou!');
-    // res.send(new_name); Tava dando erro pq nao pode dar res.send nada dps de renderizar header.
-    res.render('dashboard');
-	}
-);
->>>>>>> 3751149853d40f6fffd4876508015a2d23a19f5f
 
 
 
@@ -85,10 +49,6 @@ app.post('/login', function (req, res) {
 
 
 
-<<<<<<< HEAD
 app.listen(3000, function() {
 	console.log("Listening on port 3000");
 });
-=======
-app.listen(3000);
->>>>>>> 3751149853d40f6fffd4876508015a2d23a19f5f
